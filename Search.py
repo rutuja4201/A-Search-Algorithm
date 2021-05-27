63601b0846c7396276a114bc837537850ee7969c
Rutuja <rutusmore2001@gmail.com>
2021-05-28 03:24:54 +0530

    added Search.py file

---
+++ Search.py
@@ -0,0 +1,36 @@
+import pygame
+import math
+from queue import PriorityQueue
+
+WIDTH = 800
+WIN = pygame.display.set_mode((WIDTH, WIDTH))
+pygame.display.set_caption("A* Path Finding Algorithm")
+
+RED = (255, 0, 0)
+BLUE = (0, 255, 0)
+GREEN = (0, 0, 255)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165, 0)
+GREY = (128, 128, 128)
+TURQUOISE = (64, 224, 208)
+
+class Spot:
+	def __init__(self, row, col, width, total_rows):
+		self.row = row
+		self.col = col
+		self.x = row = width
+		self.y = col = width
+		self.color = WHITE
+		self.neighbors = []
+		self.width = width
+		self.total_rows = total_rows
+
+	def get_pos(self):
+		return self.row, self.col 
+
+	def is_closed(self):
+		return self.color == RED
+		
